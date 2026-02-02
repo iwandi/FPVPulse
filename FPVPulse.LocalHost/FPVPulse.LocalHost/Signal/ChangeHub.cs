acb3763917390e38424f90ccf38506ecd028dd73
Andreas Ackermann <ackermanna@gmx.de>
2026-02-02 19:07:29 +0100

    Add Singnal System

---
+++ FPVPulse.LocalHost/FPVPulse.LocalHost/Signal/ChangeHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FPVPulse.LocalHost.Signal
+{
+    public class ChangeHub : Hub
+    {
+        public async Task Subscribe(ChangeGroup group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group.ToString());
+        }
+
+        public async Task Unsubscribe(ChangeGroup group)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group.ToString());
+        }
+    }
+}
---
+++ FPVPulse.LocalHost/FPVPulse.LocalHost/Signal/ChangeSignaler.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FPVPulse.LocalHost.Signal
+{
+    public enum ChangeGroup
+    {
+        None,
+        InjestEvent,
+        InjestRace,
+        InjestPilotResult
+    }
+
+    public class ChangeEventArgs : EventArgs
+    {
+        public ChangeGroup Group { get; }
+        public int Id { get; }
+
+        public ChangeEventArgs(ChangeGroup group, int id)
+        {
+            Group = group;
+            Id = id;
+        }
+    }
+
+    public class ChangeSignaler
+    {
+        public event EventHandler<ChangeEventArgs>? OnChange;
+        public event EventHandler<ChangeEventArgs>? OnInjestEventChanged;
+        public event EventHandler<ChangeEventArgs>? OnInjestRaceChanged;
+        public event EventHandler<ChangeEventArgs>? OnInjestPilotResultChanged;
+
+        readonly IHubContext<ChangeHub> changeHub;
+
+        public ChangeSignaler(IHubContext<ChangeHub> changeHub)
+        {
+            this.changeHub = changeHub;
+        }
+
+        public async Task SignalChangeAsync(ChangeGroup group, int id)
+        {
+            var change = new ChangeEventArgs(group, id);
+            await SignalChangeAsync(change);
+        }
+
+        public async Task SignalChangeAsync(ChangeEventArgs change)
+        {
+            OnChange?.Invoke(this, change);
+            switch(change.Group)
+            {
+                case ChangeGroup.InjestEvent:
+                    OnInjestEventChanged?.Invoke(this, change);
+                    break;
+                case ChangeGroup.InjestRace:
+                    OnInjestRaceChanged?.Invoke(this, change);
+                    break;
+                case ChangeGroup.InjestPilotResult:
+                    OnInjestPilotResultChanged?.Invoke(this, change);
+                    break;
+            }
+
+            await changeHub.Clients.Group(change.Group.ToString())
+                .SendAsync("Change", change.Group, change.Id);
+        }
+    }
+}
