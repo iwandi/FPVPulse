65a32ef8448a406d5028e2c0a400b8977a3749a6
Andreas Ackermann <ackermanna@gmx.de>
2026-02-12 02:52:10 +0100

    Detect Invalid Races !!! Bad Performance !!!

--- FPVPulse.LocalHost/FPVPulse.LocalHost/Generator/RaceValidCheckTransformer.cs
+++ FPVPulse.LocalHost/FPVPulse.LocalHost/Generator/RaceValidCheckTransformer.cs
@@ -1,14 +1,19 @@
-﻿using FPVPulse.LocalHost.Client.Components.Data;
+﻿using FPVPulse.Ingest;
+using FPVPulse.LocalHost.Client.Components.Data;
 using FPVPulse.LocalHost.Injest.Db;
 using FPVPulse.LocalHost.Signal;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
 
 namespace FPVPulse.LocalHost.Generator
 {
 	public class RaceValidCheckTransformer : BaseTransformer<Race>
 	{
+		readonly ILogger<RaceValidCheckTransformer> logger;
+
 		public RaceValidCheckTransformer(ChangeSignaler changeSignaler, IServiceProvider serviceProvider) : base(changeSignaler, serviceProvider)
 		{
-
+			logger = serviceProvider.GetRequiredService<ILogger<RaceValidCheckTransformer>>();
 		}
 
 		public override void Bind(ChangeSignaler changeSignaler)
@@ -44,12 +49,79 @@ namespace FPVPulse.LocalHost.Generator
 
 			var hasChanges = await db.SaveChangesAsync() > 0;
 
-
 			// DANGER : this is a selve invokation.
-			if (hasChanges)
-				await changeSignaler.SignalChangeAsync(Client.ChangeGroup.Race, id, parentId, data);
+			/*if (hasChanges)
+				await changeSignaler.SignalChangeAsync(Client.ChangeGroup.Race, id, parentId, data);*/
+
+			await CheckSequence(db, data);
 
 			return true;
+		}
+
+		async Task CheckSequence(EventDbContext db, Race data)
+		{
+			logger.LogInformation($"Check Sequence: {data.RaceId}");
+
+			var raceSequence = await db.Races.Where(e => e.EventId == data.EventId && e.RaceType == RaceType.Mains)
+				.OrderByDescending(e => e.FirstOrderPoistion)
+				.ThenByDescending(e => e.SecondOrderPosition)
+				.ThenByDescending(e => e.RaceId)
+				.Select(e => new { e.RaceId, e.FirstOrderPoistion, e.SecondOrderPosition, e.RaceLayout }).ToListAsync();
+
+			if (raceSequence == null || raceSequence.Count <= 1)
+				return;
+
+			var infos = raceSequence.ToArray();
+
+			var lastRace = infos.FirstOrDefault();
+			RaceLayout raceLayot = lastRace.RaceLayout;
+			int raceCount = lastRace.FirstOrderPoistion > lastRace.SecondOrderPosition ? lastRace.FirstOrderPoistion : lastRace.SecondOrderPosition;
+
+			int bottomValidId = 0;
+			int nextExpectedId = raceCount;
+			HashSet<int> invalidIds = new ();
+			foreach (var raceInfo in infos)
+			{
+				if(nextExpectedId <= 0)
+				{
+					invalidIds.Add(raceInfo.RaceId);
+					continue;
+				}
+				if (raceLayot != raceInfo.RaceLayout)
+				{
+					invalidIds.Add(raceInfo.RaceId);
+					continue;
+				}
+				if(raceInfo.FirstOrderPoistion == nextExpectedId || raceInfo.SecondOrderPosition == nextExpectedId)
+				{
+					nextExpectedId--;
+					bottomValidId = raceInfo.RaceId;
+				}
+				else
+				{
+					invalidIds.Add(raceInfo.RaceId);
+				}
+			}
+
+
+			logger.LogInformation($"Check stats: infos.Length:{infos.Length}, topValidId:{raceCount}, bottomValidId:{bottomValidId}, invalidIds.Count:{invalidIds.Count}");
+
+			bool hasChange = false;
+			foreach (var raceInfo in infos)
+			{
+				bool invalid = invalidIds.Contains(raceInfo.RaceId);
+				var race = await db.Races.Where( e => e.RaceId == raceInfo.RaceId && e.Invalid != invalid).FirstOrDefaultAsync();
+				if (race != null)
+				{
+					race.Invalid = invalid;
+					hasChange = true;
+				}
+			}
+
+			if (hasChange)
+			{
+				var hasChanges = await db.SaveChangesAsync() > 0;
+			}
 		}
 	}
 }
